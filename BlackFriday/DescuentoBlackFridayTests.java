e8726bbb3134cce5add0f0a41ce28a584ea0389b
Rocio <rociodiazf@gmail.com>
2020-12-16 16:25:03 +0100

    Añadidos test No Black Friday

--- BlackFriday/DescuentoBlackFridayTests.java
+++ BlackFriday/DescuentoBlackFridayTests.java
@@ -253,6 +253,230 @@ public class DescuentoBlackFridayTests
   }
 
   //AHORA REPETIMOS TEST, CUANDO NO SEA EL DIA DE BLACKFRIDAY
+  // Test en función del tipo de parámetro
 
+  //test que implementa C1: B1.1 y C2 : B3.1
+  @Test
+  public void test_b11_b31_NoBK()
+  {
+
+   pOriginal = -5.00;
+   porcDescuento = -10.00;
+
+   try {
+     DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+   }catch (IllegalArgumentException e) {
+       return;
+   }
+   fail ("Illegal Argument Exception expected");
+  }
+
+  //test que implementa C1: B1.2 y C2 : B3.1
+  @Test
+  public void test_b12_b31_NoBK()
+  {
+   pOriginal = 80.00;
+   porcDescuento = -10.00;
+
+   try {
+     DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+   }catch (IllegalArgumentException e) {
+      return;
+  }
+  fail ("Illegal Argument Exception expected");
+  }
+
+  //test que implementa C1: B1.1 y C2 : B3.2
+  @Test
+  public void test_b11_b32_NoBK()
+  {
+   pOriginal = -80.00;
+   porcDescuento = 50.00;
+
+   try {
+     DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+   }catch (IllegalArgumentException e) {
+      return;
+  }
+  fail ("Illegal Argument Exception expected");
+  }
+
+  //test que implementa C1: B1.1 y C2 : B4
+  @Test
+  public void test_b11_b5_NoBK()
+  {
+   pOriginal = -80.00;
+   porcDescuento = 0.00;
+
+   try {
+     DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento,dameFechaHoy());
+   }catch (IllegalArgumentException e) {
+      return;
+  }
+  fail ("Illegal Argument Exception expected");
+  }
+
+  //test que implementa C1: B4 y C2 : B3.1
+  @Test
+  public void test_b4_b31_NoBK()
+  {
+   pOriginal = 0.00;
+   porcDescuento = -80.00;
+
+   try {
+     DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento,dameFechaHoy());
+   }catch (IllegalArgumentException e) {
+      return;
+  }
+  fail ("Illegal Argument Exception expected");
+  }
+
+
+ //test que implementa C1: B1.2 y C2 : B3.2
+ @Test
+ public void test_b12_b32_NoBK()
+ {
+ pOriginal = 80.00;
+ porcDescuento = 50.00;
+
+ assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 80.00, DELTA);
+ }
+
+ //test que implementa C1: B2 y C2 : B3.2
+ @Test
+ public void test_b2_b32_NoBK()
+ {
+  pOriginal = 0.00;
+  porcDescuento = 50.00;
+  assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 0.00, DELTA);
+ }
+
+ //test que implementa C1: B1.2 y C2 : B4
+ @Test
+ public void test_b12_b4_NoBK()
+ {
+  pOriginal = 45.00;
+  porcDescuento = 0.00;
+  assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 45.00, DELTA);
+ }
+
+ //test que implementa C1: B2 y C2 : B4
+ @Test
+ public void test_b2_b4_NoBK()
+ {
+  pOriginal = 0.00;
+  porcDescuento = 0.00;
+  assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 0.00, DELTA);
+ }
+
+ //Segun el porcentaje de descuento...
+
+ //test que implementa C1: B1.2 y C3 : B5.1
+ @Test
+ public void test_b12_b51_NoBK()
+ {
+  pOriginal = 50.00;
+  porcDescuento = 150.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+
+ //caso frontera
+ @Test
+ public void test_CasoFrontera1_NoBK()
+ {
+  pOriginal = 50.00;
+  porcDescuento = 100.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+
+
+
+ //Test que implementa una caso frontera
+ @Test
+ public void test_CasoFrontera2_NoBK()
+ {
+    pOriginal = 50.00;
+    porcDescuento = 0.00;
+    assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 50.00, DELTA);
+
+ }
+
+
+ //Duda ¿Son REALMENTE necesarios?
+ //Ya se calcula el error de cada uno de ellos por separado en los test anteriores
+ //Test que implementa la caracterización del bloque b1.1 y b2.2.1
+ @Test
+ public void test_b11_b221_NoBK()
+ {
+  pOriginal = -50.00;
+  porcDescuento = 200.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+
+ //Test que implementa la caracterización del bloque b1.1 y b2.2.2
+ @Test
+ public void test_b11_b222_NoBK()
+ {
+  pOriginal = -50.00;
+  porcDescuento = 10.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+ //Test que implementa la caracterización del bloque b1.1 y b2.2.2
+ //Caso frontera
+ @Test
+ public void test_b11_b221_forentera1_NoBK()
+ {
+  pOriginal = -50.00;
+  porcDescuento = 100.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+ //Test que implementa la caracterización del bloque b1.1 y b2.2.2
+ //Caso frontera
+ @Test
+ public void test_b11_b221_frontera2_NoBK()
+ {
+  pOriginal = -50.00;
+  porcDescuento = 100.00;
+  try {
+    DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy());
+  }catch (IllegalArgumentException e) {
+     return;
+  }
+  fail ("Illegal Argument Exception expected");
+ }
+ // ES dia de blackFriday
+ @Test
+ public void test_29Nov_NoBK()
+ {
+    pOriginal = 50.00;
+    porcDescuento = 0.00;
+    assertEquals(DescuentoBlackFriday.precioFinal(pOriginal, porcDescuento, dameFechaHoy()), 50.00, DELTA);
+
+ }
 
 }
